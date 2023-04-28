b06ba6b8ec0b99dbb14badc26bbafd8b2e7d3349
ameena08 <ameenaami101@gmail.com>
2023-04-28 17:26:42 +0530

    finally

--- src/main/java/com/UST/EmployeeRegistration/service/EmployeeService.java
+++ src/main/java/com/UST/EmployeeRegistration/service/EmployeeService.java
@@ -22,7 +22,7 @@ public class EmployeeService {
 	private EmployeeRepo repo;
 
 	public Employee createEmployee(Employee employee) {
-		
+
 		return repo.save(employee);
 	}
 
@@ -35,28 +35,31 @@ public class EmployeeService {
 		// TODO Auto-generated method stub
 		return repo.findById(id).orElse(null);
 	}
-	
-	public  String deleteEmployeeById(int id) {
+
+	public String deleteEmployeeById(int id) {
 		// TODO Auto-generated method stub
 		repo.deleteById(id);
 		return "the employee information is deleted";
 	}
 
-	public Employee updateEmployee(int id,Employee employee) {
+	public Employee updateEmployee(int id, Employee employee) {
 		// TODO Auto-generated method stub
-		Employee oldemp=null;
-		Optional<Employee>optionalemployee=repo.findById(id);
-		if(optionalemployee.isPresent()) {
-			oldemp=optionalemployee.get();
-			oldemp.setEmpname(employee.getEmpname());
+//		Employee oldemp=null;
+//		Optional<Employee>optionalemployee=repo.findById(id);
+//		if(optionalemployee.isPresent()) {
+//			oldemp=optionalemployee.get();
+//			oldemp.setEmpname(employee.getEmpname());
 //			oldemp.setAddress(employee.getAddress());
 //			oldemp.setLocation(employee.getLocation());
-			
-			repo.save(oldemp);
-		}else {
-			return new Employee();
-		}
-		return oldemp;
+//
+//			repo.save(oldemp);
+//		}else {
+//			return new Employee();
+//		}
+//		return oldemp;
+//	}
+//
+		employee.setId(id);
+		return repo.save(employee);
 	}
-
 }
